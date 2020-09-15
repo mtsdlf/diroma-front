67101e22a242dd9d52087ac3cc28e658e7f85384
mtsdlf <mts.dlf@gmail.com>
2020-09-15 05:27:55 -0300

    asd

--- src/Vue.config.js
+++ src/Vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/": {
-        target: "http://104.248.7.235:3000",
+        target: "104.248.7.235:3000",
         ws: true,
         changeOrigin: true
       }
